29bab1cca1c440fb13d096f1c310381c7b0a0bb3
Vivekanand Rao <vivek.rao@teachscape.com>
2015-12-29 15:59:22 -0800

    Created the Media Object.

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ var playlist = new Playlist();
 var hello = new Song("Hello","Adele","6:07");
 var listen = new Song("Listen","Beyonce","3:05");
 var black = new Song("Back To Black","Amy Winehouse","4:09");
-var temptations = new Song("Temptations","Silent Night","6:10");
+var danza = new Song("Danza Kuduro ft. Lucenzo","Don Omar","3:55");
 var dont = new Song("Don\'t","Ed Sheeran","4:52");
 var fallin = new Song("Fallin","Alicia Keys","3:28");
 
 playlist.add(hello);
 playlist.add(listen);
 playlist.add(black);
-playlist.add(temptations);
+playlist.add(danza);
 playlist.add(dont);
 playlist.add(fallin);
 
--- song.js
+++ song.js
@@ -1,18 +1,3 @@
-function Song(title, artist, duration) {
-  this.title = title;
-  this.artist = artist;
-  this.duration = duration;
-  this.isPlaying = false;
-}
-
-Song.prototype.play = function(){
-  this.isPlaying = true;
-};
-
-Song.prototype.stop = function(){
-  this.isPlaying = false;
-};
-
 Song.prototype.toHTML = function(){
   var htmlString = '<li';
   if(this.isPlaying){

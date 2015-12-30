d7fe7d9beac257837d4a34535ca3ef95a94cf074
Vivekanand Rao <vivek.rao@teachscape.com>
2015-12-29 16:05:19 -0800

    Updated Song object to inherit from the Media object.

--- song.js
+++ song.js
@@ -1,3 +1,10 @@
+function Song(title, artist, duration) {
+  Media.call(this, title, duration);
+  this.artist = artist;
+}
+
+Song.prototype = Object.create(Media.prototype);
+
 Song.prototype.toHTML = function(){
   var htmlString = '<li';
   if(this.isPlaying){

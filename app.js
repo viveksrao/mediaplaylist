b222818a732fc5ad2aa01fbd144ac64a6a3b1483
Vivekanand Rao <vivek.rao@teachscape.com>
2015-12-29 16:21:54 -0800

    Built the Movie Object.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var playlist = new Playlist();
 
+// List of Songs
 var hello = new Song("Hello","Adele","6:07");
 var listen = new Song("Listen","Beyonce","3:05");
 var black = new Song("Back To Black","Amy Winehouse","4:09");
@@ -13,6 +14,14 @@ playlist.add(black);
 playlist.add(danza);
 playlist.add(dont);
 playlist.add(fallin);
+
+// List of Movies
+
+var hateful8 = new Movie("The Hateful Eight","2015","3:07:00");
+var starwars = new Movie("Star Wars: The Force Awakens","2015","2:15:00");
+
+playlist.add(hateful8);
+playlist.add(starwars);
 
 var playlistElement = document.getElementById('playlist');
 playlist.renderInElement(playlistElement);

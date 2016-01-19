2eeeeec534fee38812b657bea49a22b9a8ae9078
Vivekanand Rao <vivek.rao@teachscape.com>
2016-01-19 11:12:25 -0800

    Built Quiz WebApp.

--- app.js
+++ app.js
@@ -1,43 +1,8 @@
-var playlist = new Playlist();
+var questions = [
+  new Question("Who was the first President of the United States?",["George Washington","Thomas Jefferson"],"George Washington"),
+  new Question("What is the answer to the Ultimate Question of Life, the Universe, and Everything?",["Pi","42"],"42")
+];
 
-// List of Songs
-var hello = new Song("Hello","Adele","6:07");
-var listen = new Song("Listen","Beyonce","3:05");
-var black = new Song("Back To Black","Amy Winehouse","4:09");
-var danza = new Song("Danza Kuduro ft. Lucenzo","Don Omar","3:55");
-var dont = new Song("Don\'t","Ed Sheeran","4:52");
-var fallin = new Song("Fallin","Alicia Keys","3:28");
+var quiz = new Quiz(questions);
 
-playlist.add(hello);
-playlist.add(listen);
-playlist.add(black);
-playlist.add(danza);
-playlist.add(dont);
-playlist.add(fallin);
-
-// List of Movies
-
-var hateful8 = new Movie("The Hateful Eight","2015","3:07:00");
-var starwars = new Movie("Star Wars: The Force Awakens","2015","2:15:00");
-
-playlist.add(hateful8);
-playlist.add(starwars);
-
-var playlistElement = document.getElementById('playlist');
-playlist.renderInElement(playlistElement);
-
-var playButton = document.getElementById('play');
-playButton.onclick = function(){
-  playlist.play();
-  playlist.renderInElement(playlistElement);
-}
-var nextButton = document.getElementById('next');
-nextButton.onclick = function(){
-  playlist.next();
-  playlist.renderInElement(playlistElement);
-}
-var stopButton = document.getElementById('stop');
-stopButton.onclick = function(){
-  playlist.stop();
-  playlist.renderInElement(playlistElement);
-}
+QuizUI.displayNext();
